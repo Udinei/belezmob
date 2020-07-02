964fa7e6dd7ef60376793d9effd06e50b6083eae
Udinei <ukaliko@gmail.com>
2020-07-02 16:55:32 -0400

    Configurações iniciais do projetot

---
+++ src/index.js
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import './config/ReactrotonConfig';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+
+console.tron.log('Wello World');
+
+export default function App() {
+    return (
+        <View style={ styles.container }>
+            <Text style={ styles.welcome }>Hello World </Text>
+        </View>
+    );
+}
