ca27ca7e9cff11e29926592215cbfb283d724f5a
Udinei <ukaliko@gmail.com>
2020-08-11 11:31:14 -0400

    inclusao url api remota heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 ﻿import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://10.0.2.2:3333',
+    //baseURL: 'http://10.0.2.2:3333',
+    baseURL: 'https://belez-api.herokuapp.com',
 });
 
 export default api;
