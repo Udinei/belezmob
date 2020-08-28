4dd7e3a72f82dda2fa38d46c12028915e6d136b0
Udinei <ukaliko@gmail.com>
2020-08-28 16:11:31 -0400

    Validação de horario ja agendando

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://10.0.2.2:3333',
+    baseURL:'https://belez-api.herokuapp.com'
     //__DEV__ ? 'http://10.0.2.2:3333' : 'https://belez-api.herokuapp.com',
 });
 
