1e3eef27dc17ecaaa1d9eafc9bff057ff2f0690c
Udinei <ukaliko@gmail.com>
2020-10-03 19:25:50 -0400

    ...

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.0.2.2:3333'
+    baseURL: 'http://159.203.106.82'
+    // baseURL: 'http://10.0.2.2:3333'
     //__DEV__ ? 'http://10.0.2.2:3333' : 'https://belez-api.herokuapp.com',
 });
 
